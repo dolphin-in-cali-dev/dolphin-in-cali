152971049a1ebbb763b19cf86e8c3bb7b27736d0
yugyeomkim <hoodie0512@gmail.com>
2025-01-26 21:39:53 +0900

    chore: Setup glsl loader

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,13 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(frag|vert)$/,
+      type: 'asset/source',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
