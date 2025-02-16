64b73149fc0f628cc41f17e306482c120a712670
yugyeomkim <hoodie0512@gmail.com>
2025-02-16 20:36:11 +0900

    add: Empty page for Rooftop project

--- app/components/GlobalTabs.tsx
+++ app/components/GlobalTabs.tsx
@@ -1,17 +1,34 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/lib/utils';
 
 const GlobalTabs = () => {
+  const pathname = usePathname();
+  const isRooftop = pathname.split('/')?.[1] === 'rooftop';
+
   return (
     <header className="mx-auto flex w-full max-w-[1440px] gap-x-2 px-10">
       <Link
         href="/"
-        className="rounded-b-lg bg-neutral-900 px-2 pb-2 pt-3 text-sm font-extrabold text-white"
+        className={cn(
+          'rounded-b-lg px-2 pb-2 pt-3 text-sm font-extrabold',
+          isRooftop ? 'text-neutral-500' : 'bg-neutral-900 text-white',
+        )}
       >
         AGENCY
       </Link>
-      <div className="cursor-pointer px-2 pb-2 pt-3 text-sm font-extrabold text-neutral-500">
+      <Link
+        href="/rooftop"
+        className={cn(
+          'rounded-b-lg px-2 pb-2 pt-3 text-sm font-extrabold',
+          isRooftop ? 'bg-neutral-900 text-white' : 'text-neutral-500',
+        )}
+      >
         ROOFTOP
-      </div>
+      </Link>
     </header>
   );
 };
---
+++ app/rooftop/page.tsx
@@ -0,0 +1,10 @@
+const RoofTopPage = () => {
+  return (
+    <main className="mx-auto max-w-[1440px] px-10 pb-20 pt-10">
+      Welcome to the Rooftop Page!
+      <p>This page gives you an overview of our rooftop features.</p>
+    </main>
+  );
+};
+
+export default RoofTopPage;
